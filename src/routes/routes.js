9d6d343c478f1ff65f534c85a856a73decd05dfd
LuisUD <llemt_53@hotmail.com>
2020-08-24 09:06:57 -0500

    desactivación de jwt para pruebas

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -21,24 +21,24 @@ module.exports = app => {
   app.post("/api/login", autorizacion.login)
 
   //Rutas Usuarios
-  app.get("/api/usuarios", autorizacion.autorizar(Rol.Administrador), usuarioController.obtenerUsuarios)
-  app.get("/api/usuario/:id", autorizacion.autorizar(Rol.Administrador), usuarioController.obtenerUsuario)
-  app.post("/api/usuario", validadorUsuario.crearUsuarioSchema, usuarioController.crearUsuario)
-  app.put("/api/usuario", autorizacion.autorizar(Rol.Administrador), usuarioController.actualizarUsuario)
-  app.delete("/api/usuario/:id", autorizacion.autorizar(Rol.Administrador), usuarioController.eliminarUsuario)
+  app.get("/api/usuarios", /*autorizacion.autorizar(Rol.Administrador),*/ usuarioController.obtenerUsuarios)
+  app.get("/api/usuario/:id", /*autorizacion.autorizar(Rol.Administrador),*/ usuarioController.obtenerUsuario)
+  app.post("/api/usuario", /*validadorUsuario.crearUsuarioSchema,*/ usuarioController.crearUsuario)
+  app.put("/api/usuario", /*autorizacion.autorizar(Rol.Administrador),*/ usuarioController.actualizarUsuario)
+  app.delete("/api/usuario/:id", /*autorizacion.autorizar(Rol.Administrador),*/ usuarioController.eliminarUsuario)
 
   //Rutas Tiendas
   app.get("/api/tiendas", tiendaController.obtenerTiendas)
   app.get("/api/tienda/:id", tiendaController.obtenerTienda)
-  app.post("/api/tienda", autorizacion.autorizar([Rol.Vendedor,Rol.Administrador]), tiendaController.crearTienda)
-  app.put("/api/tienda", autorizacion.autorizar([Rol.Vendedor,Rol.Administrador]), tiendaController.actualizarTienda)
-  app.delete("/api/tienda/:id", autorizacion.autorizar([Rol.Vendedor,Rol.Administrador]), tiendaController.eliminarTienda)
+  app.post("/api/tienda", /*autorizacion.autorizar([Rol.Vendedor,Rol.Administrador]),*/ tiendaController.crearTienda)
+  app.put("/api/tienda", /*autorizacion.autorizar([Rol.Vendedor,Rol.Administrador]),*/ tiendaController.actualizarTienda)
+  app.delete("/api/tienda/:id", /*autorizacion.autorizar([Rol.Vendedor,Rol.Administrador]),*/ tiendaController.eliminarTienda)
 
   //Rutas Productos
   app.get("/api/productos", productoController.obtenerProductos)
   app.get("/api/producto/:id", productoController.obtenerProducto)
-  app.post("/api/producto", autorizacion.autorizar([Rol.Vendedor,Rol.Administrador]), productoController.crearProducto)
-  app.put("/api/producto", autorizacion.autorizar([Rol.Vendedor,Rol.Administrador]), productoController.actualizarProducto)
-  app.delete("/api/producto/:id", autorizacion.autorizar([Rol.Vendedor,Rol.Administrador]), productoController.eliminarProducto)
+  app.post("/api/producto", /*autorizacion.autorizar([Rol.Vendedor,Rol.Administrador]),*/ productoController.crearProducto)
+  app.put("/api/producto", /*autorizacion.autorizar([Rol.Vendedor,Rol.Administrador]),*/ productoController.actualizarProducto)
+  app.delete("/api/producto/:id", /*autorizacion.autorizar([Rol.Vendedor,Rol.Administrador]),*/ productoController.eliminarProducto)
 
 }
