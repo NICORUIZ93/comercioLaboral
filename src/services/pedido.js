a5870535dc33f57c33e3bb91eabe9aca2b6e77a7
LuisUD <llemt_53@hotmail.com>
2020-09-15 12:53:42 -0500

    ajuste

--- src/services/pedido.js
+++ src/services/pedido.js
@@ -61,7 +61,7 @@ const service = {
           valorTotal: 0,
           confirmado: false,
           uuid: uuid,
-          valorTotal: productos.reduce((a, b) => a.valor + b.valor, 0)
+          valorTotal: productos.reduce((a, b) => a + b.valor, 0)
           //Detalle: detallesPedido,
         };
 
