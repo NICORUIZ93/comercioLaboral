b2e50dd02f635a84f84fdedf74c1f2af1a66bfa1
LuisUD <llemt_53@hotmail.com>
2020-09-15 15:13:02 -0500

    ajuste error

--- src/db/models/detallepedido.js
+++ src/db/models/detallepedido.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       
-      this.belongsTo(models.Rol, {
+      this.belongsTo(models.Producto, {
         foreignKey: "IdProducto"
       });
 
