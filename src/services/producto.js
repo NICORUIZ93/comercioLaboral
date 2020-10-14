e3a90bc0f8c4c3f32a152d48a48c4689069255a2
LuisUD <llemt_53@hotmail.com>
2020-10-13 22:28:49 -0500

    pruebas

--- src/services/producto.js
+++ src/services/producto.js
@@ -286,7 +286,7 @@ const service = {
         },
         order: [["createdAt", "ASC"]],
       });
-
+/*
       const porductosFiltrados = productos.rows.map((p) => {
         const { Tiendas, ...producto } = p.dataValues;
         producto.IdTienda = Tiendas[0].id;
@@ -296,7 +296,7 @@ const service = {
       });
 
       productos.rows = porductosFiltrados;
-
+*/
       return paginador.paginarDatos(productos, pagina, limit);
     } catch (error) {
       throw error;
