3b7165e7ef3fb866f7eb3cc1fced50b5efb96977
dilanmartinez <dilankmilo11@gmail.com>
2021-04-20 13:01:33 -0500

    make it better

--- src/services/cargarArchivosS3.js
+++ src/services/cargarArchivosS3.js
@@ -4,10 +4,10 @@ const multerS3 = require("multer-s3");
 const util = require("util");
 const admin = require('firebase-admin');
 const serviceAccount = require('../files/lamejorferia-32065-firebase-adminsdk-m4ddl-4823e442cf.json');
-    admin.initializeApp({
-      credential: admin.credential.cert(serviceAccount)
-    });
-    const db = admin;
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+const db = admin;
 
 const s3Bucket = new aws.S3({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
@@ -80,13 +80,17 @@ const service = {
   },
   async storage(req) {
 
-    const bucket =   admin.storage().bucket('gs://lamejorferia-32065.appspot.com')
-    let storageRef = await admin.storage().bucket('gs://lamejorferia-32065.appspot.com').upload('./archivos/' + req.file.originalname, { resumable: true, public: true });
-    console.log(storageRef)
-    let file = bucket.file(req.file.originalname)
-    const publicUrl = file.publicUrl()
-    console.log(publicUrl)
-    return publicUrl
+    try {
+      const bucket = admin.storage().bucket('gs://lamejorferia-32065.appspot.com')
+      let storageRef = await admin.storage().bucket('gs://lamejorferia-32065.appspot.com').upload('./archivos/' + req.file.originalname, { resumable: true, public: true });
+      console.log(storageRef)
+      let file = bucket.file(req.file.originalname)
+      const publicUrl = file.publicUrl()
+      console.log(publicUrl)
+      return publicUrl
+    } catch (error) {
+        return error  
+    }
 
   },
 };
