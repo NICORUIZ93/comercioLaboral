83fd3d8c3857d5e4db3edc5f68714e33f2ed6f4c
dilanmartinez <dilankmilo11@gmail.com>
2021-04-30 14:32:04 -0500

    make it better

---
+++ src/constants/filtroComentarios.js
@@ -0,0 +1,27 @@
+let malas_palabras = [
+    "hpta",
+    "perra",
+    "puta",
+    "puto",
+    "marica",
+    "pirobo",
+    "gonorrea",
+    "perro",
+    "hpta",
+    "perra",
+    "gordo",
+    "flaco",
+    "viejo",
+    "hijo de puta",
+    "verga",
+    "pene",
+    "vagina",
+    "triplehijueputa",
+    "hijueputa",
+    "mierda",
+    "m13d4d4",
+    "feo",
+
+]
+
+module.exports = { malas_palabras }
--- src/services/calificacionProducto.js
+++ src/services/calificacionProducto.js
@@ -4,100 +4,106 @@ const tienda = require('../db/models').Tienda;
 const respuestaTienda = require('../db/models').respuestaTienda
 const { Op } = require("sequelize");
 var sequelize = require("../db/models").sequelize;
-
+const {malas_palabras} = require('../constants/filtroComentarios')
 const service = {
-    
-    
-    async calificacionProductos(nuevaCalificacion) {
-      try {
-        const usu = await Usuario.findAll({
-          attributes: ['id','nombre'],
-          where: {
-            'id': nuevaCalificacion.IdUsuario,
-          }
-        });
-        let resultadocreate = "";
-        if ((JSON.parse(JSON.stringify(usu)))[0] != undefined) {
-           resultadocreate = await calificacionProductos.create(
-            nuevaCalificacion
-          );
-        }else {
-            resultadocreate = "No existe el usuario"
-        }
-    
-  
-        return resultadocreate;
-      } catch (error) {
-        console.log(`${error}`);
-        throw error;
+
+
+  async calificacionProductos(nuevaCalificacion) {
+    try {
+      var grocerias = malas_palabras 
+      for (var i = 0; i < grocerias.length; i++) {
+        regex = new RegExp("(^|\\s)" + grocerias[i] + "($|(?=\\s))", "gi")
+        nuevaCalificacion.comentario = nuevaCalificacion.comentario.replace(regex, function ($0, $1) { return $1 + "comerzio" });
       }
-    },
-    async obtenerCalificacionesProducto(params) {
-      try {
-        const calificaciones = await calificacionProductos.findAll({
-          where: {
-            [Op.or]: params,
-          },
-          order: [
+      
+      const usu = await Usuario.findAll({
+        attributes: ['id', 'nombre'],
+        where: {
+          'id': nuevaCalificacion.IdUsuario,
+        }
+      });
+      let resultadocreate = "";
+      if ((JSON.parse(JSON.stringify(usu)))[0] != undefined) {
+        resultadocreate = await calificacionProductos.create(
+          nuevaCalificacion
+        );
+      } else {
+        resultadocreate = "No existe el usuario"
+      }
+
+
+      return resultadocreate;
+    } catch (error) {
+      console.log(`${error}`);
+      throw error;
+    }
+  },
+  async obtenerCalificacionesProducto(params) {
+    try {
+      const calificaciones = await calificacionProductos.findAll({
+        where: {
+          [Op.or]: params,
+        },
+        order: [
           ['createdAt', 'DESC']
         ]
+      });
+      let cl = JSON.parse(JSON.stringify(calificaciones))
+      let resul = [];
+      for (let i = 0; i <= cl.length - 1; i++) {
+        const usu = await Usuario.findAll({
+          attributes: ['id', 'nombre'],
+          where: {
+            'id': cl[i]['IdUsuario'],
+          }
         });
-        let cl = JSON.parse(JSON.stringify(calificaciones))
-        let resul = [];
-        for (let i = 0; i <= cl.length-1; i++) {
-          const usu = await Usuario.findAll({
-            attributes: ['id','nombre'],
-            where: {
-              'id': cl[i]['IdUsuario'],
-            }
-          });
 
-          const respuesta = await respuestaTienda.findAll({
-            where: {
-              'id_calificacion': cl[i]['id'],
-            }
-          });
+        const respuesta = await respuestaTienda.findAll({
+          where: {
+            'id_calificacion': cl[i]['id'],
+          }
+        });
 
-          
 
-          console.log(respuesta)
-          resul[i] = {
-            id : cl[i]['id'],
-            id_producto : cl[i]['IdProducto'],
-            usuario : (JSON.parse(JSON.stringify(usu)))[i]['nombre'],
-            calificacion : cl[i]['calificacion'],
-            comentario : cl[i]['comentario'],
-            respuestas : { "datos" : JSON.parse(JSON.stringify(respuesta))},
-            createdAt : cl[i]['createdAt'],
-            updatedAt : cl[i]['updatedAt']
-          } 
+
+        console.log(respuesta)
+        resul[i] = {
+          id: cl[i]['id'],
+          id_producto: cl[i]['IdProducto'],
+          usuario: (JSON.parse(JSON.stringify(usu)))[i]['nombre'],
+          calificacion: cl[i]['calificacion'],
+          comentario: cl[i]['comentario'],
+          respuestas: { "datos": JSON.parse(JSON.stringify(respuesta)) },
+          createdAt: cl[i]['createdAt'],
+          updatedAt: cl[i]['updatedAt']
+        }
+      }
+
+      console.log(resul)
+      return resul;
+    } catch (error) {
+      console.log(`${error}`);
+      throw error;
+    }
+  },
+  async obtenerPromedioProducto(params) {
+    try {
+      const calificaciones = await calificacionProductos.findAll({
+        attributes: [
+          [sequelize.fn('AVG', sequelize.col('calificacion')), 'promedio_calificacion'],
+        ], where: {
+          [Op.or]: params,
         }
 
-        console.log(resul)
-        return resul;
-      } catch (error) {
-        console.log(`${error}`);
-        throw error;
-      }
-    },
-    async obtenerPromedioProducto(params) {
-      try {
-        const calificaciones = await calificacionProductos.findAll({
-          attributes: [
-            [sequelize.fn('AVG', sequelize.col('calificacion')), 'promedio_calificacion'],
-            ], where: {
-              [Op.or]: params,
-            }
-          
-        });
-  
-        return calificaciones;
-      } catch (error) {
-        console.log(`${error}`);
-        throw error;
-      }
+      });
+
+      return calificaciones;
+    } catch (error) {
+      console.log(`${error}`);
+      throw error;
     }
-   
-  };
-  
+  }
+
+};
+
 module.exports.calificacionProductos = service;
