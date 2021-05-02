d8432f031e05e750b43fa21cb27d367ffd226ce5
dilanmartinez <dilankmilo11@gmail.com>
2021-05-02 18:40:26 -0500

    make it better

--- src/services/calificacionProducto.js
+++ src/services/calificacionProducto.js
@@ -66,26 +66,41 @@ const service = {
             'id': cl[i]['IdUsuario'],
           }
         });
-
+       
         const respuesta = await respuestaTienda.findAll({
           where: {
             'id_calificacion': cl[i]['id'],
           }
         });
 
-
-
-        console.log(respuesta)
-        resul[i] = {
-          id: cl[i]['id'],
-          id_producto: cl[i]['IdProducto'],
-          usuario: (JSON.parse(JSON.stringify(usu)))[i]['nombre'],
-          calificacion: cl[i]['calificacion'],
-          comentario: cl[i]['comentario'],
-          respuestas: { "datos": JSON.parse(JSON.stringify(respuesta)) },
-          createdAt: cl[i]['createdAt'],
-          updatedAt: cl[i]['updatedAt']
+        if ((JSON.parse(JSON.stringify(usu)))[i] != undefined) {
+          console.log(respuesta)
+          resul[i] = {
+            id: cl[i]['id'],
+            id_producto: cl[i]['IdProducto'],
+            usuario: (JSON.parse(JSON.stringify(usu)))[i]['nombre'],
+            calificacion: cl[i]['calificacion'],
+            comentario: cl[i]['comentario'],
+            respuestas: { "datos": JSON.parse(JSON.stringify(respuesta)) },
+            createdAt: cl[i]['createdAt'],
+            updatedAt: cl[i]['updatedAt']
+          }
+        } else {
+          console.log(respuesta)
+          resul[i] = {
+            id: cl[i]['id'],
+            id_producto: cl[i]['IdProducto'],
+            usuario: "",
+            calificacion: cl[i]['calificacion'],
+            comentario: cl[i]['comentario'],
+            respuestas: { "datos": JSON.parse(JSON.stringify(respuesta)) },
+            createdAt: cl[i]['createdAt'],
+            updatedAt: cl[i]['updatedAt']
+          }
         }
+
+
+  
       }
 
       console.log(resul)
