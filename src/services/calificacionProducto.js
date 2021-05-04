51ec2e809f7b1c9e84151015695181bff70d9d56
dilanmartinez <dilankmilo11@gmail.com>
2021-05-04 10:29:14 -0500

    make it better

--- src/services/calificacionProducto.js
+++ src/services/calificacionProducto.js
@@ -141,7 +141,8 @@ const service = {
       if ((JSON.parse(JSON.stringify(tiendaProducto)))[0] != undefined ) {
         for (let i = 0; i < (JSON.parse(JSON.stringify(tiendaProducto))).length -1; i++) {
          console.log((JSON.parse(JSON.stringify(tiendaProducto)))[i]['IdProducto'])
-         consultaCalificaciones = await axios.get(`https://secure-atoll-67302.herokuapp.com/api/producto/calificaciones/${(JSON.parse(JSON.stringify(tiendaProducto)))[i]['IdProducto']}`)
+         let id = parseInt((JSON.parse(JSON.stringify(tiendaProducto)))[i]['IdProducto'])
+         consultaCalificaciones = await axios.get(`https://secure-atoll-67302.herokuapp.com/api/producto/calificaciones/${id}`)
          console.log(consultaCalificaciones.data)
          datos[i] = consultaCalificaciones.data  
         }
