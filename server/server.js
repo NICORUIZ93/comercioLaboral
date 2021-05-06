bb776b01d5f3555212737533e7b7fe58348038a9
dilanmartinez <dilankmilo11@gmail.com>
2021-05-06 16:53:05 -0500

    make it better

--- server/server.js
+++ server/server.js
@@ -4,8 +4,9 @@ const helmet = require('helmet')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const errorHandler = require('../src/helpers/error-handler');
-
 const app = express()
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
 
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
@@ -13,11 +14,15 @@ app.use(helmet())
 app.use(cors())
 
 
+io.on('connection' , (socket) => {
+    const idConexion = socket.id
+    console.log("Conexion establecida con " + idConexion)
+})
 
 require("../src/routes/routes")(app)
 
 app.use(errorHandler);
 
-app.listen(config.PORT, function () {
+server.listen(config.PORT, function () {
     console.log(`App listening on port:${config.PORT}`);
 });
