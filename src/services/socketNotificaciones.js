b110262c1f4035656e615bab75d29fbac8df7307
dilanmartinez <dilankmilo11@gmail.com>
2021-05-07 16:03:55 -0500

    make it better

--- src/services/socketNotificaciones.js
+++ src/services/socketNotificaciones.js
@@ -4,14 +4,14 @@ const socketService = async(io) => {
         const idSocket = socket.id
         console.log("Usuario conectado  = " +idSocket)
         usuarios.push(idSocket)
-        io.emit('activos' , usuarios)
+        socket.emit('activos' , usuarios)
 
         socket.on('disconnect', function() {
             console.log('Got disconnect!');
       
             var i = usuarios.indexOf(socket);
             usuarios.splice(i, 1);
-            io.emit('activos' , usuarios)
+            socket.emit('activos' , usuarios)
          });
 
        
