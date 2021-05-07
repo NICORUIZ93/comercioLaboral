1e1b0ebe29f253992ab116c677810f645878a3ff
dilanmartinez <dilankmilo11@gmail.com>
2021-05-07 14:25:21 -0500

    make it better

--- src/services/socketNotificaciones.js
+++ src/services/socketNotificaciones.js
@@ -2,7 +2,6 @@ const socketService = async(io) => {
     io.on("connection", socket => {
         const idSocket = socket.id
         console.log("Usuario conectado  = " +idSocket)
-        io.emit('conectados' , idSocket )
     })
 
 
