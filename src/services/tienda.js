9884b7dd553617cadcc973594339f393c260d5ee
dilanmartinez <dilankmilo11@gmail.com>
2021-05-11 15:11:27 -0500

    make it better

--- src/services/tienda.js
+++ src/services/tienda.js
@@ -99,7 +99,7 @@ const service = {
 
       let nProductos = await TiendaProducto.count({  where : {  'IdTienda' : idTienda } })
 
-      return tiendaSinUsuarios + nProductos;
+      return tiendaSinUsuarios + {nProductos};
     } catch (error) {
       console.log(`${error}`);
       throw error;
