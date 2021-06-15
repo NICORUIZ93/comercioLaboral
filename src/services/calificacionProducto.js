c0738a1a943361cdd6f49d968ce3c12467ea3a8f
dilanmartinez <dilankmilo11@gmail.com>
2021-06-15 17:37:18 -0500

    make it better

--- src/services/calificacionProducto.js
+++ src/services/calificacionProducto.js
@@ -29,7 +29,7 @@ const service = {
       const pe = await Pedido.findAll({
         attributes: ['id'],
         where: {
-          'id': nuevaCalificacion.IdProducto
+          'id': nuevaCalificacion.IdPedido
         }
       });
 
